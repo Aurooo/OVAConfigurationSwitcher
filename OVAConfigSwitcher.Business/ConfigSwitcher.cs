45ce23d88ac1cf129db9cbb33d3164d55077cd80
Aurooo <aurom140@gmail.com>
2021-10-28 12:22:54 +0200

    implemented IConfigSwitcher

---
+++ OVAConfigSwitcher.Business/ConfigSwitcher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OVAConfigSwitcher.Business.Contracts.Models;
+using OVAConfigSwitcher.Business.Contracts.Interfaces;
+using System.IO;
+
+namespace OVAConfigSwitcher.Business
+{
+    public class ConfigSwitcher : IConfigSwitcher
+    {
+        private readonly string _rootPath;
+        private readonly string _currentConfiguration;
+
+        public ConfigSwitcher(string rootPath, string currentConfiguration)
+        {
+            _rootPath = rootPath ?? throw new ArgumentNullException(nameof(rootPath));
+            _currentConfiguration = currentConfiguration ?? throw new ArgumentNullException(nameof(currentConfiguration));
+        }
+
+        public bool ApplyConfigurationFile(AgencyConfigurationFile agencyConfigurationFile)
+        {
+            if (!File.Exists(agencyConfigurationFile.FilePath))
+            {
+                throw new FileNotFoundException($"file does not exist: {nameof(agencyConfigurationFile.FilePath)}");
+            }
+
+            File.Copy(agencyConfigurationFile.FilePath, _currentConfiguration, true);
+
+            return true;
+        }
+
+        public IEnumerable<AgencyConfigurationFile> GetAgencyConfigurationFiles(string environmentName)
+        { 
+            var configurationFiles = new List<AgencyConfigurationFile>();
+
+            var configurationFileList = Directory.GetFiles(Path.Combine(_rootPath, environmentName), "*.*", SearchOption.TopDirectoryOnly)
+                .Where(file => file.EndsWith(".xml") || file.EndsWith(".json"));
+
+            foreach (var configurationFile in configurationFileList)
+            {
+                configurationFiles.Add(new AgencyConfigurationFile(configurationFile));
+            }
+
+            return configurationFiles;
+        }
+
+        public IEnumerable<EnvironmentType> GetEnvironments()
+        {
+            var environments = new List<EnvironmentType>();
+
+            foreach (var environment in Directory.GetDirectories(_rootPath))
+            {
+                environments.Add(new EnvironmentType(environment));
+            };
+
+            return environments;
+        }
+    }
+}
--- RegistryReader/RegistryStream.cs
+++ RegistryReader/RegistryStream.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using RegistryReader.Interfaces;
 using RegistryReader.Models;
+
 namespace RegistryReader
 {
     public class RegistryStream
