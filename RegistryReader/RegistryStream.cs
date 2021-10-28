b39d215f8ea8c6762ab9f4f7a01f260a85f651f3
Aurooo <aurom140@gmail.com>
2021-10-28 11:14:39 +0200

    added actual registry reader class

---
+++ RegistryReader/RegistryStream.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using RegistryReader.Interfaces;
+using RegistryReader.Models;
+namespace RegistryReader
+{
+    public class RegistryStream
+    {
+        public IEnumerable<RegistryValueElement> Read(string registryKey)
+        {
+            IRegistryReader registryReader = new RegistryFactory().GetBaseKey(registryKey);
+            return registryReader.Read(registryKey);
+        }
+    }
+}
