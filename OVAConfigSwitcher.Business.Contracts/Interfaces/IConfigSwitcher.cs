4745a4a50887133100dbb262c77006efca800c23
Aurooo <aurom140@gmail.com>
2021-10-28 11:41:08 +0200

    added interface and models for env and agency config files

---
+++ OVAConfigSwitcher.Business.Contracts/Interfaces/IConfigSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OVAConfigSwitcher.Business.Contracts.Models;
+
+namespace OVAConfigSwitcher.Business.Contracts.Interfaces
+{
+    public interface IConfigSwitcher
+    {
+        IEnumerable<EnvironmentType> GetEnvironments();
+        IEnumerable<AgencyConfigurationFile> GetAgencyConfigurationFiles(string environmentName);
+        bool ApplyConfigurationFile(AgencyConfigurationFile agencyConfigurationFile);
+    }
+}
---
+++ OVAConfigSwitcher.Business.Contracts/Models/AgencyConfigurationFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OVAConfigSwitcher.Business.Contracts.Models
+{
+    public class AgencyConfigurationFile
+    {
+        public string AgencyFileName { get; set; }
+        public string FilePath { get; set; }
+        public string EnvironmentName { get; set; }
+
+        public AgencyConfigurationFile(string filePath)
+        {
+            var directoryPath = Path.GetDirectoryName(filePath);
+
+            AgencyFileName = filePath.Replace(directoryPath + Path.DirectorySeparatorChar, "");
+            FilePath = filePath;
+            EnvironmentName = directoryPath.Replace(Path.GetDirectoryName(directoryPath), "");
+        }
+    }
+}
