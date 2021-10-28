a0e0cbfd8f0009770b0e539966abc25485da0606
Aurooo <aurom140@gmail.com>
2021-10-28 18:00:23 +0200

    added appsettings.json and bound it with appsettings

--- OVAConfigSwitcher.App/Appl.cs
+++ OVAConfigSwitcher.App/Appl.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OVAConfigSwitcher.Business;
 
 namespace OVAConfigSwitcher.App
 {
@@ -22,8 +23,26 @@ namespace OVAConfigSwitcher.App
 
         public void Run(string[] args)
         {
+            ValidateArgs(args);
 
+            var currentConfig = new RegistryStream().Read(_appSettings.RegistryKey)
+                .Where(element => element.Name == "ConfigFilePath1")
+                .Select(element => element).FirstOrDefault().Value;
+
+            var configSwitcher = new ConfigSwitcher(_appSettings.RootDirectory, currentConfig);
+
+            // complete switch in configuration
         }
 
+        private static void ValidateArgs(string[] args)
+        {
+            if (!(args.Length == 2))
+                throw new ArgumentException(nameof(args));
+
+            if (!args[0].StartsWith("-") && "pre/prod/dev".Contains(args[0]))
+                throw new ArgumentException(nameof(args));
+
+            //finish validating for xml file (arg[1])
+        }
     }
 }
--- OVAConfigSwitcher.App/Program.cs
+++ OVAConfigSwitcher.App/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.IO;
+using RegistryReader;
 
 namespace OVAConfigSwitcher.App
 {
