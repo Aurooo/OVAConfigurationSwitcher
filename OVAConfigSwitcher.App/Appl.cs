3f7fe59e9d2d7c7e5263ee79b502be4798e53677
Aurooo <aurom140@gmail.com>
2021-10-29 17:09:17 +0200

    added args overload: list envs, list files, apply configuration

--- OVAConfigSwitcher.App/Appl.cs
+++ OVAConfigSwitcher.App/Appl.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using OVAConfigSwitcher.Business;
 using System.IO;
+using OVAConfigSwitcher.Business.Contracts.Models;
 
 namespace OVAConfigSwitcher.App
 {
@@ -13,62 +14,76 @@ namespace OVAConfigSwitcher.App
     {
         private readonly ILogger<Appl> _logger;
         private readonly AppSettings _appSettings;
+        private ConfigSwitcher ConfigSwitcher { get; set; }
 
         public Appl(IOptions<AppSettings> appSettings, ILogger<Appl> logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _appSettings = appSettings?.Value ?? throw new ArgumentNullException(nameof(appSettings));
+            ConfigSwitcher = InitialiseConfigSwitcher();
         }
 
         public void Run(string[] args)
         {
-            ValidateArgs(args);
+            var environments = ConfigSwitcher.GetEnvironments().ToList();
+            var usage = "\n\rUse -e to list available environments.\n\r" +
+                "Use -f <envName> to list available configuration files in <envName>.\n\r" +
+                "Use -a <envName> <configFile> to apply new configuration.\n\r";
 
+            if (args.Length == 1 && args[0] == "-e")
+            {
+                environments.ForEach(env => Console.WriteLine(env.EnvironmentName));
+                Console.ReadLine();
+
+                return;
+            }
+
+            if (args.Length == 2 && args[0] == "-f" && IsValidEnvironment(args[1]))
+            {
+                ConfigSwitcher.GetAgencyConfigurationFiles(args[1]).ToList().ForEach(file => Console.WriteLine(file.AgencyFileName));
+                Console.ReadLine();
+
+                return;
+            }
+
+            if (args.Length == 3 && args[0] == "-a" && IsValidEnvironment(args[1]) && IsValidConfigurationFile(args[1], args[2]))
+            {
+                var agencyConfigurationFile = ConfigSwitcher.GetAgencyConfigurationFiles(args[1])
+                    .Where(file => file.AgencyFileName == args[2])
+                    .Select(file => file).Single();
+
+                ConfigSwitcher.ApplyConfigurationFile(agencyConfigurationFile);
+
+                _logger.LogInformation($"Now using: {args[1]} => {agencyConfigurationFile.AgencyFileName}. Configuration switch succesful");
+                Console.ReadLine();
+
+                return;
+            }
+
+            _logger.LogError(usage);
+            Console.ReadLine();
+
+            return;
+        }
+        private ConfigSwitcher InitialiseConfigSwitcher()
+        {
             var currentConfig = new RegistryStream().Read(_appSettings.RegistryKey)
                 .Where(element => element.Name == "ConfigFilePath1")
                 .Select(element => element).FirstOrDefault().Value;
 
-            var configSwitcher = new ConfigSwitcher(_appSettings.RootDirectory, currentConfig);
-
-            // complete switch in configuration
+            return new ConfigSwitcher(_appSettings.RootDirectory, currentConfig);
         }
 
-        private void ValidateArgs(string[] args)
+        private bool IsValidConfigurationFile(string environment, string agencyConfigurationFile)
         {
-            if (!(args.Length == 2))
-                throw new ArgumentException("Provide 2 arguments: env configfile");
-
-            //get envs from root
-            if (!args[0].StartsWith("-") && "pre/prod/test".Contains(args[0]))
-                throw new ArgumentException("Not a valid environment");
-
-            //finish validating for xml file (arg[1])
-            if (!args[1].Contains(".xml") && !args[1].Contains(".json"))
-                throw new ArgumentException("Not an xml or json file");
-
-            if (!ConfigurationExists(_appSettings, args))
-                throw new FileNotFoundException($"{args[1]} not found in {args[0]}");
+            return ConfigSwitcher.GetAgencyConfigurationFiles(environment).Where(file => file.AgencyFileName == agencyConfigurationFile)
+                .Select(env => env.AgencyFileName).ToList().Count > 0;
         }
 
-        private bool ConfigurationExists(AppSettings appSettings, string[] args)
+        private bool IsValidEnvironment(string environment)
         {
-            string environment = appSettings.RootDirectory;
-
-            switch (args[0])
-            {
-                case "-test":
-                    environment = Path.Combine(environment, "test");
-                    break;
-                case "-pre":
-                    environment = Path.Combine(environment, "pre_prod");
-                    break;
-                case "-prod":
-                    environment = Path.Combine(environment, "prod");
-                    break;
-            }
-
-            return File.Exists(Path.Combine(environment, args[1]));
-
+            return ConfigSwitcher.GetEnvironments().Where(env => env.EnvironmentName == environment)
+                .Select(env => env.EnvironmentName).ToList().Count > 0;
         }
     }
 }
